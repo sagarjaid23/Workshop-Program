d418e9c83bd086533dd041cce0ed2a4e4cede919
sagarjaid23 <sagarjaid23@gmail.com>
2022-04-06 10:17:25 +0530

    Prime Numbers in given range

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c=0;
+            Console.WriteLine("Enter a Start number");
+            a= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a End number");
+            b= Convert.ToInt32(Console.ReadLine());
+            for (int i = a; i < b; i++)
+            {
+                for(int j = 1; j < b; j++)
+                {
+                    if(i%j==0)
+                    {
+                        c++;
+                    }
+                }
+                if (c==2)
+                {
+                    
+                    Console.WriteLine(i);
+                }
+                c = 0;
+            }
+
+
+        }
+    }
+}
